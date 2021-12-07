c8705e5fdc47bde2187049d56b4304828060cb06
RafaelSantana11 <rafael@araguaiatec.com>
2021-12-07 19:37:36 -0300

    criando middleware de error e ajustes nos controllers e services de usuario

---
+++ src/middlewares/globalErrors.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import AppError from '../shared/error/AppError';
+
+function globalErrors(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  if (err instanceof AppError) {
+    res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+      data: err?.data,
+    });
+  }
+
+  console.log(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+}
+
+export { globalErrors };
--- src/resources/user/user.service.ts
+++ src/resources/user/user.service.ts
@@ -4,6 +4,7 @@ import { User } from '../../entity/User';
 
 import { UserSignIn } from './dtos/user.signin.dtos';
 import { UserSignUp } from './dtos/user.signup.dtos';
+import AppError from '../../shared/error/AppError';
 
 export default class UserService {
   async signin(user: UserSignIn) {
@@ -13,12 +14,14 @@ export default class UserService {
     const passwordHash = md5(password).toString();
 
     const existUser = await userRepository.findOne({
-      where: { email, password: passwordHash},
+      where: { email, password: passwordHash },
     });
 
     if (!existUser) {
-      return null;
+      throw new AppError('Usuário não encontrado', 401);
     }
+
+    return existUser;
   }
 
   async signup(user: UserSignUp) {}
